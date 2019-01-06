78b5a7cf5a872d700cc7357f384c4566f63f20d5
vksingh26 <vksingh20@outlook.com>
2019-01-06 23:37:01 +0530

    Added package.json and created server program

--- Node/app.js
+++ Node/app.js
@@ -1,1 +1,15 @@
 /* created by vikash Kumar Singh */
+/* Creating node server using http modules */
+
+const http = require('http');
+const hostname = 'localhost';
+const port = 9000;
+http.createServer(function(request, response) {
+    response.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+    response.write("Learning node js !");
+    response.end();
+}).listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
