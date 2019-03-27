8fd33157d059cac175310a170da79fb539202949
Vikash Kumar Singh <VikashS1@verifone.com>
2019-03-27 20:16:19 +0530

    added some fs logic and form submission logic

--- Node/app.js
+++ Node/app.js
@@ -3,16 +3,29 @@
 
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 const hostname = 'localhost';
 const port = 9000;
 http.createServer(function(request, response) {
+    const url = request.url;
+    const method = request.method;
+    if(url === '/'){
+        response.write('<!DOCTYPE html><html><body><h2> HTML Forms</h2><form action = "/submitUser" method="POST">First name: <br><input type = "text" name = "firstname"><br><br><input type = "submit" value = "Submit"></form><p> If you click the "Submit"button, the form - data will be sent to a page called "/action_page.php". </p></body></html>');
+        return response.end();
+    }
+
+    if (url === '/submitUser' && method == 'POST') {
+        fs.writeFileSync('../Node/src/formResponse.txt', 'Started learning node js and this file has been created by using nodejs!');
+        response.statusCode = 302;
+        response.statusMessage = 'Message written successfully!';
+        response.setHeader('location', '/');
+        return response.end();
+    }
     response.writeHead(200, {
-        "Content-Type": "text/plain"
+        "Content-Type": "text/html"
     });
-    response.write("Learning node js !");
+    response.write('<!DOCTYPE html><html><head><meta charset = "UTF-8" ><title> Title of the document </title></head><body><h1> Node js loaded...... </h1></body></html>');
     response.end();
 }).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
-const myURL = url.parse('https://user:pass@sub.example.com:8080/p/a/t/h?query=string#hash');
-console.log("URL PARSED" + JSON.stringify(myURL));
