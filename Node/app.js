71df9f51827ebce3169ada4e088030297a6e12d4
Vikash Kumar Singh <VikashS1@verifone.com>
2019-03-27 18:39:02 +0530

    added module

--- Node/app.js
+++ Node/app.js
@@ -2,6 +2,7 @@
 /* Creating node server using http modules */
 
 const http = require('http');
+const url = require('url');
 const hostname = 'localhost';
 const port = 9000;
 http.createServer(function(request, response) {
@@ -13,3 +14,5 @@ http.createServer(function(request, response) {
 }).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+const myURL = url.parse('https://user:pass@sub.example.com:8080/p/a/t/h?query=string#hash');
+console.log("URL PARSED" + JSON.stringify(myURL));
