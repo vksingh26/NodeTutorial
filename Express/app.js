16b7da92eeb186dc80ead2fd69eca7f220cb7ba3
Vikash Kumar Singh <VikashS1@verifone.com>
2019-04-25 16:02:21 +0530

    complete crud operation for shopping cart

--- Express/app.js
+++ Express/app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById('5cc05dbf60a7d406c04d4163')
       .then(user => {
-        req.user = user;
+        req.user = new User(user.name, user.email, user.cart, user._id);
         next();
       })
       .catch(err => console.log(err));
